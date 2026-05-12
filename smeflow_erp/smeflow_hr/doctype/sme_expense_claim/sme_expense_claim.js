ba50b9f41946de7714c81e312bee83d2fc21609f
Francess Ekezie <zynefrancess6444@gmail.com>
2026-05-12 11:28:10 +0100

    Add SMEFlow HR staff roles and permissions

---
+++ smeflow_erp/smeflow_hr/doctype/sme_expense_claim/sme_expense_claim.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Francess Ekezie and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("SME Expense Claim", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ smeflow_erp/smeflow_hr/doctype/sme_payroll/test_sme_payroll.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Francess Ekezie and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSMEPayroll(FrappeTestCase):
+	pass
